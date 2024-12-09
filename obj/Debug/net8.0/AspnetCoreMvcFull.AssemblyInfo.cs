73fdba4f95f39df78b7e13d945af1fc4b7d90eeb
alongduck <tris25094@gmail.com>
2024-12-09 12:17:57 +0700

    update pwa

--- obj/Debug/net8.0/AspnetCoreMvcFull.AssemblyInfo.cs
+++ obj/Debug/net8.0/AspnetCoreMvcFull.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4db3ea7d21396d6774a23ef774a943cc0a4b23")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60f51f4f4074161ae3b7cd0e729ba185ad88a412")]
 [assembly: System.Reflection.AssemblyProductAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AspnetCoreMvcFull")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
